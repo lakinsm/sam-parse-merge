b43b5214f6687d3fcdbf40b005c59d7fec060d20
lakinsm <steven.m.lakin@gmail.com>
2021-10-04 14:51:59 -0500

    Debugging args

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,45 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <map>
 #include "args.h"
 
 
 int main(int argc, const char *argv[]) {
     Args args(argc, argv);
 
-    std::ifstream ifs(args.sam_file_list, std::ios::in);
+    // Load SAM file paths
+    // Format: [PATH1, PATH2, PATH3, ... ]
+    std::ifstream ifs1(args.sam_file_list, std::ios::in);
     std::string line, sam_filepath;
-    std::stringstream ss;
+    std::stringstream ss1;
 
     std::vector< std::string > sam_files;
 
-    std::getline(ifs, line);
+    std::getline(ifs1, line);
     line.erase(0, 1);
     line.erase(line.size() - 1);
     line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
 
-    ss.str(line);
-    while(std::getline(ss, sam_filepath, ',')) {
+    ss1.str(line);
+    while(std::getline(ss1, sam_filepath, ',')) {
         sam_files.push_back(sam_filepath);
     }
 
-    std::cout << std::endl;
-    for( auto &x : sam_files ) {
-        std::cout << x << std::endl;
+    // Load Barcode to top genome mapping
+    std::ifstream ifs2(args.best_genomes, std::ios::in);
+    line.clear();
+    std::string barcode, genome;
+    std::stringstream ss2;
+
+    std::map< std::string, std::string > best_genomes;
+
+    while(std::getline(ifs1, line)) {
+        ss2.str(line);
+        std::getline(ss2, barcode, '\t');
+        std::getline(ss2, genome, '\t');
+        best_genomes[barcode] = genome;
+        std::cout << barcode << '\t' << genome << std::endl;
     }
 
     return 0;
