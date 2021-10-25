6bc0a975df4cc3fc21571648bf89c8167e9edf2d
lakinsm <steven.m.lakin@gmail.com>
2021-10-25 15:08:51 -0500

    Debugging barcode

--- src/concurrent_buffer_queue.cpp
+++ src/concurrent_buffer_queue.cpp
@@ -39,7 +39,6 @@ void ConcurrentBufferQueue::run()
                 _barcode_out_list.push_back(barcode);
                 std::string out_filepath = barcode + "_aligned_reads.sam";
                 _ofs_out.emplace_back(std::ofstream{out_filepath});
-                std::cout << barcode << std::endl;
                 _ofs_out[idx] << _headers.at(barcode);
             }
 
