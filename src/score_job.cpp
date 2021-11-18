22fb56e13cef5dd1b07344ff655394ec610e96e4
lakinsm <steven.m.lakin@gmail.com>
2021-11-18 12:47:56 -0600

    Debugging new pipelines

--- src/score_job.cpp
+++ src/score_job.cpp
@@ -186,7 +186,7 @@ void ScoreJob::_samScore(std::ifstream &ifs, const std::string &initial_line)
     std::vector< std::string > res;
     int read_idx = 0;
     int sam_flag;
-    print(initial_line);
+    std::cout << initial_line << std::endl;
     res = _parseSamLine(initial_line);
     if((res.size() == 0) || (res[0].empty())) {
         return;
